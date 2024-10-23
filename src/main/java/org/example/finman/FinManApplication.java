f2ffd507d6bc5e094faa53b8a57401c595d0a231
Mohsen <mohsenr7596@hotmail.com>
2024-10-23 19:55:20 +0330

    Init Commit

---
+++ src/main/java/org/example/finman/FinManApplication.java
@@ -0,0 +1,13 @@
+package org.example.finman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinManApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinManApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/finman/FinManApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.finman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinManApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
